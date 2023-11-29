3e4e0ce0331eedfbe9788662503c71c073230be8
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-11-29 14:41:44 +0000

    modular code

--- src/unsupervised_llm_embeddings.py
+++ src/unsupervised_llm_embeddings.py
@@ -1,6 +1,8 @@
 import torch
 import pandas as pd
 import os
+import time
+import logging
 
 from argparse import ArgumentParser
 from utils import read_config
@@ -10,6 +12,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 print(f"Device: {device}")
+
+
+PROMPTS = {1: 'The concept "<CONCEPT>" means in one word: "'}
 
 
 # base_model_id_13b = "meta-llama/Llama-2-13b-chat-hf"
@@ -27,7 +32,15 @@ tokenizer = AutoTokenizer.from_pretrained(
 tokenizer.pad_token = tokenizer.eos_token
 
 
-def get_embeddings(input_list):
+def get_embeddings(input_list, prompt_id):
+    concept_prompts = [
+        PROMPTS[prompt_id].replace("<CONCEPT>", con) for con in input_list
+    ]
+
+    logging.info(concept_prompts[0:15])
+
+    embeddings = dict()
+
     for i, idx in enumerate(range(0, len(input_list), batch_size)):
         inputs = tokenizer.batch_encode_plus(
             batch_text_or_text_pairs=concept_prompts[idx : idx + batch_size],
@@ -49,53 +62,70 @@ def get_embeddings(input_list):
 
 
 def get_data(config):
-    data_dir = config["data_dir"]
-
-    train_file = os.path.join(data_dir, "train_mcrae.tsv")
-
-    test_file = os.path.join(data_dir, "test_mcrae.tsv")
-
-    train_df = pd.read_csv(train_file, sep="\t", names=["concept", "property", "label"])
-    test_df = pd.read_csv(test_file, sep="\t", names=["concept", "property", "label"])
-
     def clean_text(text):
         return " ".join(text.replace("_", " ").split())
 
-    train_df["concept"] = train_df["concept"].apply(clean_text)
-    train_df["property"] = train_df["property"].apply(clean_text)
+    all_concepts = set()
 
-    test_df["concept"] = test_df["concept"].apply(clean_text)
-    test_df["property"] = test_df["property"].apply(clean_text)
+    if config.get("train_file") is not None:
+        train_file = config["train_file"]
+        train_df = pd.read_csv(
+            train_file, sep="\t", names=["concept", "property", "label"]
+        )
+        train_df["concept"] = train_df["concept"].apply(clean_text)
+        train_df["property"] = train_df["property"].apply(clean_text)
 
-    print(f"train_df: {train_df}")
-    print(f"test_df: {test_df}")
+        logging.info(f"train_df: {train_df}")
+        all_concepts.update(train_df["concept"].unique())
 
-    print(f"train_df.shape: {train_df.shape}")
-    print(f"test_df.shape: {test_df.shape}")
+    if config.get("val_file") is not None:
+        val_file = config["val_file"]
+        val_df = pd.read_csv(val_file, sep="\t", names=["concept", "property", "label"])
 
-    all_concepts = set(
-        list(train_df["concept"].unique()) + list(test_df["concept"].unique())
-    )
-    all_properties = set(
-        list(train_df["property"].unique()) + list(test_df["property"].unique())
-    )
+        val_df["concept"] = val_df["concept"].apply(clean_text)
+        val_df["property"] = val_df["property"].apply(clean_text)
+        logging.info(f"val_df: {val_df}")
 
-    print(f"all_concepts: {len(all_concepts)}")
-    print(f"all_properties: {len(all_properties)}")
+        all_concepts.update(val_df["concept"].unique())
 
-    concept_prompt = f'The concept "<CONCEPT>" means in one word: "'
-    concept_prompts = [concept_prompt.replace("<CONCEPT>", con) for con in all_concepts]
+    if config.get("test_file") is not None:
+        test_file = config["test_file"]
+        test_df = pd.read_csv(
+            test_file, sep="\t", names=["concept", "property", "label"]
+        )
+        test_df["concept"] = test_df["concept"].apply(clean_text)
+        test_df["property"] = test_df["property"].apply(clean_text)
 
-    print(f"concept_prompts: {concept_prompts[0:10]}")
+        logging.info(f"test_df: {test_df}")
+        all_concepts.update(test_df["concept"].unique())
 
-    property_prompt = f'The property "<PROPERTY>" means in one word: "'
-    property_prompts = [
-        property_prompt.replace("<PROPERTY>", prop) for prop in all_properties
-    ]
+    logging.info(f"all_concepts: {all_concepts}")
 
-    print(f"property_prompts: {property_prompts[0:10]}")
+    return all_concepts
 
-    return concept_prompts, property_prompts
+    # # all_concepts = set(
+    # #     list(train_df["concept"].unique()) + list(test_df["concept"].unique())
+    # # )
+    # # all_properties = set(
+    # #     list(train_df["property"].unique()) + list(test_df["property"].unique())
+    # # )
+
+    # print(f"all_concepts: {len(all_concepts)}")
+    # # print(f"all_properties: {len(all_properties)}")
+
+    # concept_prompt = f'The concept "<CONCEPT>" means in one word: "'
+    # concept_prompts = [concept_prompt.replace("<CONCEPT>", con) for con in all_concepts]
+
+    # print(f"concept_prompts: {concept_prompts[0:10]}")
+
+    # property_prompt = f'The property "<PROPERTY>" means in one word: "'
+    # property_prompts = [
+    #     property_prompt.replace("<PROPERTY>", prop) for prop in all_properties
+    # ]
+
+    # print(f"property_prompts: {property_prompts[0:10]}")
+
+    # return concept_prompts, property_prompts
 
 
 if __name__ == "__main__":
@@ -108,10 +138,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
     config = read_config(args.config_file)
 
-    concept_prompts, property_prompts = get_data(config=config)
+    log_file_name = os.path.join(
+        "logs",
+        config.get("log_dirctory"),
+        f"log_{config.get('experiment_name')}_{time.strftime('%d-%m-%Y_%H-%M-%S')}.txt",
+    )
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=log_file_name,
+        filemode="w",
+        format="%(asctime)s : %(name)s : %(levelname)s - %(message)s",
+    )
+
+    concepts = get_data(config=config)
 
     print(f"concept_prompts")
-    get_embeddings(concept_prompts)
-
-    print(f"property_prompts")
-    get_embeddings(property_prompts)
+    get_embeddings(input_list=concepts, prompt_id=config["prompt_id"])
